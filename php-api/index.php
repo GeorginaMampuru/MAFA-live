7eddb5bcd672f8d552c371961d04bc18d6a95590
Georgina Mampuru <66384787+GeorginaMampuru@users.noreply.github.com>
2026-05-15 17:20:58 +0200

    Delete php-api directory

--- php-api/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode([
-    'ok' => true,
-    'message' => 'MAFA form API. Use POST contact.php or consultation.php from the website.',
-]);
